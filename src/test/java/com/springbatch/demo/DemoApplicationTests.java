3a7d04f920d425f1911fff5098ed12f08add373f
yakusugi <masterplan_life@protonmail.com>
2025-01-24 19:57:14 +0900

    for linux env 2

--- src/test/java/com/springbatch/demo/DemoApplicationTests.java
+++ src/test/java/com/springbatch/demo/DemoApplicationTests.java
@@ -1,9 +1,10 @@
 package com.springbatch.demo;
 
+import com.springbatch.chunkprocessing.ChunkProcessingApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ChunkProcessingApplication.class)
 class DemoApplicationTests {
 
 	@Test
